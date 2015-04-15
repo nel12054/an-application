76dc873000e6d0597509921abd6523a0d353619b
nicknlsn <nicknlsn@gmail.com>
2015-04-15 12:27:42 -0700

    Initial Commit

---
+++ an application/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  an application
+//
+//  Created by Nick Nelson on 4/15/15.
+//  Copyright (c) 2015 Nick Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
